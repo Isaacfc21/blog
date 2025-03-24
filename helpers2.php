e132c6a2bb9675fd6e83f8885c0c2730a1ce6eda
Isaac <isaaccaracayahoo@gmail.com>
2025-03-24 20:24:54 -0300

    blog

--- helpers2.php
+++ helpers2.php
@@ -3,7 +3,12 @@
 
 function slug(string $string): string
 {
-    return $string;
-}
+    // Usar iconv para remover acentos e caracteres especiais
+    $slug = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+    
+    // Remover caracteres especiais (aqueles não alfanuméricos, incluindo pontuação e símbolos)
+    $slug = preg_replace('/[^a-zA-Z0-9\s]/', '', $slug);
 
+    return $slug;
+}
 ?>
