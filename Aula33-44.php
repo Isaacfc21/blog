c965ec884552e2f36cf3866049b205aa4af7543b
Isaac <isaaccaracayahoo@gmail.com>
2025-03-25 00:53:20 -0300

    blog

--- Aula33-44.php
+++ Aula33-44.php
@@ -1,16 +1,68 @@
 <?php
     //Aula 033
 
+    // require_once 'sistema/configuracao.php';
+    // include_once 'helpers2.php';
+
+    // echo slug("      Adão \"Negro\" - '2022'    "). '<hr>';
+    // echo slug("Avatar          2: O Caminho da Água"). '<hr>';
+    // echo slug("Não! Não   olhe!"). '<hr>';
+    // echo slug("Sonic 2 - O   Filme"). '<hr>';
+    // echo slug("NOVA SÉRIE   NO DISNEY          +!"). '<hr>';
+    // echo slug("100 Melhores   filmes"). '<hr>';
+    // echo slug("teste   !@###$%6'%%',*.:/?/"). '<hr>';
+
+    //Conteúdo
+?>
+<?php
+    //Aula 034
+
+    // require_once 'sistema/configuracao.php';
+    // include_once 'helpers2.php';
+
+    // echo saudacao();
+
+    //Conteúdo
+?>
+<?php
+    //Aula 035
+
+    // require_once 'sistema/configuracao.php';
+    // include_once 'helpers2.php';
+
+    // echo saudacao_match();
+
+    //Conteúdo
+?>
+<?php
+    //Aula 036
+
+    // require_once 'sistema/configuracao.php';
+    // include_once 'helpers2.php';
+
+    // $numero = 5;
+
+    // // while ($numero <= 10){
+    // //     echo $numero++;
+    // // }
+
+    // for($i = 1; $i <=10; $i++){
+    //     // echo ($i % 2 ? $i.' impar' : $i.' par').'<br>';
+    //     echo $numero.' x '.$i.' = '.$numero*$i.'<br>'; 
+    // }
+
+    //Conteúdo
+?>
+<?php
+    //Aula 037
+
     require_once 'sistema/configuracao.php';
     include_once 'helpers2.php';
 
-    echo slug("Adão \"Negro\" - '2022' "). '<hr>';
-    echo slug("Avatar 2: O Caminho da Água"). '<hr>';
-    echo slug("Não! Não olhe!"). '<hr>';
-    echo slug("Sonic 2 - O Filme"). '<hr>';
-    echo slug("NOVA SÉRIE NO DISNEY+!"). '<hr>';
-    echo slug("100 Melhores filmes"). '<hr>';
-    echo slug("teste!@###$%6'%%',*.:/?/"). '<hr>';
+    $cpf = '562.564.468-36';
 
+    // echo $limparNumero = preg_replace('/[^0-9]/', '', $cpf);
+    var_dump(validarCPF($cpf));
+    
     //Conteúdo
 ?>
--- helpers2.php
+++ helpers2.php
@@ -1,6 +1,93 @@
 <?php
     #echo("Arquivo de Funções");
 
+function validarCPF(string $cpf): bool
+{
+    $cpf = limparNumero($cpf);
+
+    if(mb_strlen($cpf) != 11 || preg_match('/(\d)\1{10}/', $cpf)){
+        return false;
+    }
+    for($t = 9; $t < 11; $t++){
+        for($d = 0, $c = 0; $c < $t; $c++){
+            $d += $cpf[$c] * (($t + 1) - $c);
+        }
+        $d = ((10 * $d) % 11) % 10;
+
+        if($cpf[$c] != $d){
+            return false;
+        }
+    }
+    return true;
+}
+
+function limparNumero(string $numero): string
+{
+    return preg_replace('/[^0-9]/', '', $numero);
+}
+
+/**
+ * Saudação de acordo com o horário
+ * @return string saudação
+ */
+
+function saudacao_match(): string
+{
+    $hora = date('H');
+    // $hora = '18';
+
+    // $saudacao = match($hora){
+    //     '23' => 'Boa noite',
+    //     default => 'Bom dia'
+    // }; 
+    
+    // $saudacao = match($hora){
+    //     '18', '19', '20', '21', '22', '23', '24' => 'Boa noite',
+    // };
+    
+    $saudacao = match(true){
+        $hora >= 0 && $hora <= 5 => 'Boa madrugada',
+        $hora >= 6 and $hora <= 12 => 'Bom dia',
+        $hora >= 13 and $hora <= 18 => 'Boa tarde',
+        default => 'Boa noite'
+    }; 
+
+    return $saudacao;
+}
+
+/**
+ * Saudação de acordo com o horário
+ * @return string saudação
+ */
+
+function saudacao_switch(): string
+{
+    $hora = date('H');
+
+    switch($hora){
+        case $hora >= 0 && $hora <= 5:
+            $saudacao = "Boa madrugada";
+            break;
+        case $hora >= 6 and $hora <= 12:
+            $saudacao = "Bom dia";
+            break;
+        case $hora >= 13 and $hora <= 18:
+            $saudacao = "Boa tarde";
+            break;
+        default:
+            $saudacao = "Boa noite";
+
+    }
+
+    return $saudacao;
+}
+ 
+/**
+ * Gera URL Amigável
+ * @param string $string a ser codificada
+ * @return string slug pronto
+ */
+
 function slug(string $string): string
 {
     // Usar iconv para remover acentos e caracteres especiais
@@ -9,6 +96,15 @@ function slug(string $string): string
     // Remover caracteres especiais (aqueles não alfanuméricos, incluindo pontuação e símbolos)
     $slug = preg_replace('/[^a-zA-Z0-9\s]/', '', $slug);
 
-    return $slug;
+    $slug = strip_tags(trim($slug));
+
+    $slug = str_replace(' ', '_', $slug);
+
+    // $slug = str_replace(['_____', '____', '___', '__', '_'], '_' ,$slug);
+
+    $slug = preg_replace('/_+/', '_', $slug);
+
+
+    return strtolower($slug);
 }
 ?>
